dabe61e63d9f4f0feaacf327a3dcf4c7a96130ae
NaveenSelvakumar24 <30549885+NaveenSelvakumar24@users.noreply.github.com>
2018-01-17 16:16:49 +0530

    Create 11.C

---
+++ 11.C
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main()
+{
+int b,e;
+int r=1;
+printf("Enter a base number: ");
+scanf("%d", &b);
+printf("Enter an exponent: ");
+scanf("%d",&e);
+while (e!=0)
+{
+r*= b;
+e--;
+}
+printf("Answer=%d",r);
+return0;
+}
