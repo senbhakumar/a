f1ef9ef052189d757b6037e2a22e5e9a7dcbfd9b
NaveenSelvakumar24 <30549885+NaveenSelvakumar24@users.noreply.github.com>
2018-01-17 15:38:11 +0530

    Update 4.c

--- 4.c
+++ 4.c
@@ -2,11 +2,11 @@
 int main()
 {
 char c;
-printf("Enter a character: ");
+printf("Enter a character");
 scanf("%c",&c);
 if( (c>='a' && c<='z') || (c>='A' && c<='Z'))
-printf("%c is an alphabet",c);
+printf("It is an alphabet");
 else
-printf("%c is not an alphabet",c);
+printf("It is not an alphabet");
 return 0;
 }
