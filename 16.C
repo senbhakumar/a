99c8517b3fc74e84ece732c960fa9867d201de66
NaveenSelvakumar24 <30549885+NaveenSelvakumar24@users.noreply.github.com>
2018-01-22 16:33:12 +0530

    Update 16.C

--- 16.C
+++ 16.C
@@ -4,13 +4,13 @@ int main()
 int low, high, i, flag;
 printf("Enter two numbers(intervals): ");
 scanf("%d %d", &low, &high);
-printf("Prime numbers between %d and %d are: ", low, high);
-while (low < high)
+printf("Prime numbers between given intervals are", low, high);
+while (low<high)
 {
 flag = 0;
-for(i = 2; i <= low/2; ++i)
+for(i=2;i<=low/2;++i)
 {
-if(low % i == 0)
+if(low%i== 0)
 {
 flag = 1;
 break;
