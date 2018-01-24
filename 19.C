ddfea3f15d2767b7f18daba757cbe787fd36d282
NaveenSelvakumar24 <30549885+NaveenSelvakumar24@users.noreply.github.com>
2018-01-24 15:47:02 +0530

    Create 19.C

---
+++ 19.C
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int i,f,num;
+f=1;
+printf("Enter a number: ");
+scanf("%d",&num);
+for(i=1;i<=num;i++)
+f=f*i;
+printf("Factorial of %d is: %d",num,f);
+getch();
+}
